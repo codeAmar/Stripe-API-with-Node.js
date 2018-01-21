514078fa16aa6fbee5bb5559bbfbdadef503fef8
Amarjot singh <amarjotsingh90@yahoo.com>
2018-01-21 02:21:40 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Handling payment with Stripe API in Node.js
+
+this repository demonstrates how to collect payment for your next e-commerce website.
---
+++ app.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+const keyPublishable = process.env.PUBLISHABLE_KEY;
+const keySecret = process.env.SECRET_KEY;
+const express = require('express');
+const app = express();
+const path = require('path');
+const stripe = require("stripe")(keySecret);
+
+app.set("view engine", "pug");
+app.use(require("body-parser").urlencoded({extended: false}));
+
+app.use(express.static(path.join(__dirname,'public')));
+app.get("/", (req, res) =>
+  res.render("index.pug", {keyPublishable}));
+
+app.post("/charge", (req, res) => {
+  let amount = parseFloat((req.body.amount)*100);
+  stripe.customers.create({
+     email: req.body.stripeEmail,
+    source: req.body.stripeToken
+  })
+  .then(customer =>
+    stripe.charges.create({
+      amount,
+      description: "Sample Charge",
+         currency: "usd",
+         customer: customer.id
+    }))
+  .then(charge => res.render("charge.pug",{amount: (amount/100)}));
+});
+
+app.listen(4567);
